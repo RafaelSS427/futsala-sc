61b1fce38569bdf30448ccf1e5a6fbfa522918b7
RafaelSS427 <Rafaelss427@gmail.com>
2023-09-08 17:10:02 -0600

    Removed config lines

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     domains: ['picsum.photos']
   },
   staticPageGenerationTimeout: 300,
-  distDir: "dist"
 }
 
 module.exports = nextConfig
